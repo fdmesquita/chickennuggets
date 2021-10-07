f095be32ad3c355f9d1d79e680b68f6ed5a0a753
fdmesquita <fannydemesquita@gmail.com>
2021-10-07 20:39:12 +0200

    finish radish animation + kill

--- Assets/Scripts/WeakSpot.cs
+++ Assets/Scripts/WeakSpot.cs
@@ -1,13 +1,15 @@
-
 using UnityEngine;
 
 public class WeakSpot : MonoBehaviour
 {
     public GameObject objectToDestroy;
+    public AudioClip killSound;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            //AudioManager.instance.PlayClipAt(killSound, transform.position);
             Destroy(objectToDestroy);
         }
     }
