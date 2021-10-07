6f5e1d3ea28396ab92cef23228e8578f711ed5b3
Guigan713 <guillaume.lequin713@gmail.com>
2021-10-08 01:00:48 +0200

    first stage done

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+   public int coinsCount;
+
+   public Text coinsCountText;
+
+   public static Inventory instance;
+
+   private void Awake() 
+   {
+       if(instance != null)
+       {
+           Debug.LogWarning("Il y a plus d'une instance de Inventory dans la scène");
+           return;
+       }
+       
+       instance = this;
+   }
+
+   public void AddCoins(int count)
+   {
+       coinsCount += count;
+       coinsCountText.text = coinsCount.ToString();
+    }
+
+}
