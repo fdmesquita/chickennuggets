6f54431f52536097de743dded41f93384b04d772
Tristan <tristan.fluteaux@gmail.com>
2021-10-08 10:23:15 +0200

    Merge branch 'dev' of github.com:fdmesquita/chickennuggets into tristanStage

---
+++ Assets/PlayerHealthPillon.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerHealthPillon : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBarPillon healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
